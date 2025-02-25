7a189f138dac19523a5fa0ff8f88e58676d8ac54
shubhamdots <Shubham.Singh@dotsquares.com>
2025-02-25 11:56:46 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,6 @@
+// Create web server
+// Create a web server that listens on port 3000 and returns a list of comments in JSON format. The comments are stored in the comments.json file.
+
+// The comments.json file contains an array of comment objects. Each comment object has the following properties:
+
+// id: a unique identifier for the comment (a number)
